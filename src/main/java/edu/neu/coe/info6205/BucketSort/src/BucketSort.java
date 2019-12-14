53e90cc8be4f29df493f1fea16800584d515abb8
DezhiYang_001447703 <yang.dez@husky.neu.edu>
2019-12-14 15:20:29 -0500

    BucketSort Impl and Anal

--- src/main/java/edu/neu/coe/info6205/BucketSort/src/BucketSort.java
+++ src/main/java/edu/neu/coe/info6205/BucketSort/src/BucketSort.java
@@ -55,7 +55,6 @@ public class BucketSort <X extends Comparable<X>> {
         List<Integer> result = buckets.stream().flatMap(numbers->numbers.stream())
                 .collect(Collectors.toList());
         result.toArray(xs);
-        System.out.println(buckets.toString());
     }
 
 
@@ -74,11 +73,59 @@ public class BucketSort <X extends Comparable<X>> {
      * An example main program.
      * @param args the command-line args (ignored).
      */
+
     public static void main(String[] args) {
-        BucketSort s = new BucketSort(2);
-        Integer[] array = {5, 3, 0, 2, 4, 1, 0, 5, 2, 3, 1, 4};
-        System.out.println("Before:\t\t\t\t\t" + Arrays.toString(array));
-        s.sort(array, 0,array.length);
-        System.out.println("After:\t\t\t\t\t" + Arrays.toString(array));
+
+        /**
+         * Have a try.
+         */
+         BucketSort s = new BucketSort(2);
+         Integer[] array = {5, 3, 0, 2, 4, 1, 0, 5, 2, 3, 1, 4};
+         System.out.println("Before:\t\t\t\t\t" + Arrays.toString(array));
+         s.sort(array, 0,array.length);
+         System.out.println("After:\t\t\t\t\t" + Arrays.toString(array));
+
+        /**
+         *
+         * We analyze the effect of array size on time complexity
+         */
+//        BucketSort s = new BucketSort(512);
+//        Random random = new Random();
+//        for(int size = 10000;size<=1280000;size*=2){
+//        Integer[] array = new Integer[size];
+//        long time;
+//        long startTime = System.currentTimeMillis();
+//        for (int t = 0; t < 10; t++)
+//        {
+//            for (int i = 0; i < array.length; i++) array[i] = random.nextInt(size);
+//            s.sort(array, 0, array.length);
+//        }
+//        long endTime = System.currentTimeMillis();
+//        time = (endTime - startTime);
+//        System.out.println("Time complexity is "+time+"; Arrary is "+size+";  ^_^");}
+
+
+            /**
+            *
+            * We analyze the effect of bucket size on time complexity.
+            */
+
+//            for (int bucket = 2; bucket <= 1024; bucket *= 2) {
+//            BucketSort s = new BucketSort(bucket);
+//            Random random = new Random();
+//            Integer[] array = new Integer[320000];
+//            long time;
+//            long startTime = System.currentTimeMillis();
+//            for (int t = 0; t < 10; t++) {
+//                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(1280000);
+//                s.sort(array, 0, array.length);
+//            }
+//            long endTime = System.currentTimeMillis();
+//            time = (endTime - startTime);
+//            System.out.println("Time complexity is " + time + "; Bucket is "+bucket+";  ^_^");
+//            }
+
+
+
     }
 }
